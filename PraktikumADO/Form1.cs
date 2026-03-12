057cf6337d7d47de7292c741c32cba19ff57d04f
bimmbim <bimabaraja76@gmail.com>
2026-03-12 13:53:28 +0700

    add query count mahasiswa ExecuteScalar

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -61,6 +61,24 @@ namespace PraktikumADO
             }
         }
 
+        private void btnHitungMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+                cmd = new SqlCommand(query, conn);
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
