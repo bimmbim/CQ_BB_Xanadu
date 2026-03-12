e51b080f576a39fae9113c118abe7d2fb447dacd
bimmbim <bimabaraja76@gmail.com>
2026-03-12 14:05:51 +0700

    add form title SqlCommand ExecuteNonQuery ExecuteScalar

--- PraktikumADO/Form1.Designer.cs
+++ PraktikumADO/Form1.Designer.cs
@@ -135,7 +135,7 @@
             this.Controls.Add(this.btnHitungMhs);
             this.Controls.Add(this.btnConnect);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SqlCommand, ExecuteNonQuery, ExecuteScalar";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
