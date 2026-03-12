a7fed57cfca2883ae40eba0711d3394f11d9ee41
bimmbim <bimabaraja76@gmail.com>
2026-03-12 13:29:32 +0700

    add SQL connection variable and namespace

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PraktikumADO
 {
     public partial class Form1: Form
     {
+        SqlConnection conn;
+        SqlCommand cmd;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,6 +27,11 @@ namespace PraktikumADO
         }
 
         private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
