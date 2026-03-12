41dcd6b9a39b52c752ba9c84a90cdb857b109137
bimmbim <bimabaraja76@gmail.com>
2026-03-12 13:55:02 +0700

    implementasi update mahasiswa ExecuteNonQuery

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -79,7 +79,23 @@ namespace PraktikumADO
             }
         }
 
-
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "UPDATE Mahasiswa SET Alamat='Yogyakarta' WHERE NIM='23110100001'";
+                cmd = new SqlCommand(query, conn);
+                int hasil = cmd.ExecuteNonQuery();
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
